8270b0a0aedecf86b7e2e544c16baaa0abdd8305
outemzabetsamy <44290778+outemzabetsamy@users.noreply.github.com>
2021-03-31 22:25:36 +0100

    Update Startup.cs

--- RestApiSelami/Startup.cs
+++ RestApiSelami/Startup.cs
@@ -13,9 +13,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Data;
-using System.Data.Common;
-using System.Data.SqlClient;
 
 namespace RestApiSelami
 {
@@ -32,8 +29,7 @@ namespace RestApiSelami
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            var connectionString = GetSqlServerConnectionString();
-            services.AddDbContextPool<LieuxContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContextPool<LieuxContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LieuxContextConnectionString")));
             services.AddScoped<ILieuxData, SqlLieuxData>();
         }
 
@@ -58,49 +54,13 @@ namespace RestApiSelami
             });
         }
     }
-   
-    public static SqlConnectionStringBuilder GetSqlServerConnectionString()
+    private static void UpdateDatabase(IApplicationBuilder app){
+        using ( var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().createScope())
         {
-            // [START cloud_sql_sqlserver_dotnet_ado_connection_tcp]
-            // Equivalent connection string:
-            // "User Id=<DB_USER>;Password=<DB_PASS>;Server=<DB_HOST>;Database=<DB_NAME>;"
-            var connectionString = new SqlConnectionStringBuilder()
+            using(var context =serviceScope.ServiceProvider.GetService<MyDbContext>())
             {
-                // Remember - storing secrets in plain text is potentially unsafe. Consider using
-                // something like https://cloud.google.com/secret-manager/docs/overview to help keep
-                // secrets secret.
-                DataSource = Environment.GetEnvironmentVariable("DB_HOST"),     // e.g. '127.0.0.1'
-                // Set Host to 'cloudsql' when deploying to App Engine Flexible environment
-                UserID = Environment.GetEnvironmentVariable("DB_USER"),         // e.g. 'my-db-user'
-                Password = Environment.GetEnvironmentVariable("DB_PASS"),       // e.g. 'my-db-password'
-                InitialCatalog = Environment.GetEnvironmentVariable("DB_NAME"), // e.g. 'my-database'
-
-                // The Cloud SQL proxy provides encryption between the proxy and instance
-                Encrypt = false,
-            };
-            connectionString.Pooling = true;
-            // [START_EXCLUDE]
-            // The values set here are for demonstration purposes only. You 
-            // should set these values to what works best for your application.
-            // [START cloud_sql_sqlserver_dotnet_ado_limit]
-            // MaximumPoolSize sets maximum number of connections allowed in the pool.
-            connectionString.MaxPoolSize = 5;
-            // MinimumPoolSize sets the minimum number of connections in the pool.
-            connectionString.MinPoolSize = 0;
-            // [END cloud_sql_sqlserver_dotnet_ado_limit]
-            // [START cloud_sql_sqlserver_dotnet_ado_timeout]
-            // ConnectionTimeout sets the time to wait (in seconds) while
-            // trying to establish a connection before terminating the attempt.
-            connectionString.ConnectTimeout = 15;
-            // [END cloud_sql_sqlserver_dotnet_ado_timeout]
-            // [START cloud_sql_sqlserver_dotnet_ado_lifetime]
-            // ADO.NET connection pooler removes a connection
-            // from the pool after it's been idle for approximately
-            // 4-8 minutes, or if the pooler detects that the
-            // connection with the server no longer exists.
-            // [END cloud_sql_sqlserver_dotnet_ado_lifetime]
-            // [END_EXCLUDE]
-            return connectionString;
-            // [END cloud_sql_sqlserver_dotnet_ado_connection_tcp]
+                context.Database.Migrate();
+            }
         }
+    }
 }
